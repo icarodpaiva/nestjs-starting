a077b57f504a697834acf42f6e0a01fa5291fa3f
Icaro Paiva <icaro.paiva@acct.global>
2023-07-02 00:34:21 -0300

    feat: users controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return { body };
+  }
+
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() params) {
+    return { params, user: {} };
+  }
+
+  @Put(':id')
+  async update(@Body() body, @Param() params) {
+    return {
+      method: 'put',
+      body,
+      params,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params) {
+    return {
+      method: 'patch',
+      body,
+      params,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() params) {
+    return {
+      params,
+    };
+  }
+}
